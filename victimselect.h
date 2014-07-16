e10c4feab33a0f4f3ca394d0f75512185c031ac6
Swann Perarnau <perarnau@riken.jp>
2014-07-16 15:42:11 +0900

    Seed rng properly for all ranks.
    This is an old fix, to ensure that each rng starts with a different seed.
    Doesn't really change anything to the efficiency of various versions, it's just
    more "proper".

--- victimselect.h
+++ victimselect.h
@@ -27,6 +27,7 @@ inline int selectvictim(int rank, int size, int last)
 
 inline void vsinit(int rank, int size)
 {
+	srand(rank);
 }
 
 inline char * vsdescript(void)
@@ -62,14 +63,19 @@ inline char * vsdescript()
 
 inline void vsinit(int rank, int size)
 {
+	int i,j;
 	// allocate & init the victim array
 	victims = malloc((size -1) * sizeof(int));
-	for(int i = 0, j = 0; i < size; i++)
+	for(i = 0, j = 0; i < size; i++)
 		if(i != rank)
 			victims[j++] = i;
+	// init an rng, using the rank-th number from the global sequence as seed
 	gsl_rng_env_setup();
 	const gsl_rng_type *T = gsl_rng_default;
 	rng = gsl_rng_alloc(T);
+	for(i = 0; i < rank; i++)
+		gsl_rng_uniform_int(rng,UINT_MAX);
+	gsl_rng_set(rng,gsl_rng_uniform_int(rng,UINT_MAX));
 }
 
 inline int selectvictim(int rank, int size, int last)
@@ -91,21 +97,22 @@ gsl_ran_discrete_t *table;
 
 inline void vsinit(int rank, int size)
 {
+	int i,j;
 	// allocate & init the victim array
 	victims = malloc((comm_size -1) * sizeof(int));
-	for(int i = 0, j = 0; i < comm_size; i++)
+	for(i = 0, j = 0; i < comm_size; i++)
 		if(i != comm_rank)
 			victims[j++] = i;
 	// compute weights, using the tofu coords
 	int mx,my,mz,ma,mb,mc;
 	FJMPI_Topology_sys_rank2xyzabc(comm_rank,&mx,&my,&mz,&ma,&mb,&mc);
 	weights = malloc((comm_size -1) * sizeof(double));
-	for(int i = 0 ; i < comm_size-1; i++)
+	for(i = 0 ; i < comm_size-1; i++)
 	{
 		//find coords of this rank
 		int x,y,z,a,b,c;
 		FJMPI_Topology_sys_rank2xyzabc(victims[i],&x,&y,&z,&a,&b,&c);
-		// compute euclidian distance between nodes	
+		// compute euclidian distance between nodes
 		double d = pow(mx - x,2) + pow(my - y,2) + pow(mz - z,2) + pow(ma - a,2) + pow(mb - b,2)
 			+ pow(mc - c,2);
 #if defined(__VS_FIX__)
@@ -114,9 +121,13 @@ inline void vsinit(int rank, int size)
 		d = sqrt(d);
 		weights[i] = 1.0/d;
 	}
+	// init an rng, using the rank-th number from the global sequence as seed
 	gsl_rng_env_setup();
 	const gsl_rng_type *T = gsl_rng_default;
 	rng = gsl_rng_alloc(T);
+	for(i = 0; i < rank; i++)
+		gsl_rng_uniform_int(rng,UINT_MAX);
+	gsl_rng_set(rng,gsl_rng_uniform_int(rng,UINT_MAX));
 	table = gsl_ran_discrete_preproc(comm_size-1,weights);
 }
 
